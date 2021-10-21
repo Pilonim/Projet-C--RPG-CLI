75b16fbab107a789058e2bf64556ac0e5b51685d
Bill <bill.louischarles@gmail.com>
2021-10-21 21:49:34 +0200

    init comit

---
+++ craft.c
@@ -0,0 +1,5 @@
+//
+// Created by kerel on 21/10/2021.
+//
+
+#include "craft.h"
---
+++ craft.h
@@ -0,0 +1,8 @@
+//
+// Created by kerel on 21/10/2021.
+//
+
+#ifndef TESTOO_CRAFT_H
+#define TESTOO_CRAFT_H
+
+#endif //TESTOO_CRAFT_H
