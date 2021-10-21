e707c1ff596f4f93bbc90e7bb10071cca916c9af
Jonathan Fuentes <jonathan.fuentes@outlook.fr>
2021-10-21 21:44:00 +0200

    Add farm

---
+++ farm.c
@@ -0,0 +1,5 @@
+//
+// Created by jonat on 21/10/2021.
+//
+
+#include "farm.h"
---
+++ farm.h
@@ -0,0 +1,8 @@
+//
+// Created by jonat on 21/10/2021.
+//
+
+#ifndef PROJET_C___JEU_2D_FARM_H
+#define PROJET_C___JEU_2D_FARM_H
+
+#endif //PROJET_C___JEU_2D_FARM_H
